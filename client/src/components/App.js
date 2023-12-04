31fa25602ed8c63d681d748a9398189ea2d27861
rt00941 <ramshasaad@gmail.com>
2023-12-04 01:18:49 -0500

    feat(client-functionality): add and remove pets from placeholder list using the relevant buttons
    - list is currently not hooked up to actual data so submitting the form will refresh it along with the page but the add does work

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,51 @@
 import './App.css';
+import React from "react"
 const placeholderurl = "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg"
 const species = {
   Dog:"Dog",
   Cat:"Cat",
   Bird:"Bird"
 }
+class Pet{
+  constructor(name, pictureurl, friendly, specie){
+    this.name = name;
+    this.pictureurl = pictureurl;
+    this.friendly = friendly;
+    this.specie = specie;
+  }
+}
 var pets = [
-  { id : 1, name : "Lucky", pictureurl : placeholderurl, friendly:true, specie:species.Dog},
-  { id : 2, name : "Lucy", pictureurl : placeholderurl, friendly:false, specie:species.Bird},
+  new Pet("Lucky", placeholderurl, true, species.Dog),
+  new Pet("Lucy",placeholderurl, false, species.Bird)
 ]
 function App() {
+  const [state,setState] = React.useState({
+    petname : "",
+    petpicture : "",
+    species : undefined,
+    friendly : undefined,
+  });
+  function handleUpdateInput(event){
+    const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+    setState({
+      ...state,
+      [event.target.name] : value
+    });
+  }
+  
+  function handleAddPet(){
+    if (state.petname === "" || state.petpicture === "" || state.species === undefined){
+      console.log("required field missing");
+      return;
+    }
+    pets.push(
+    new Pet(
+      state.petname,
+      state.petpicture,
+      state.friendly,
+      state.specie
+    ));
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -21,54 +57,140 @@ function App() {
         <form>
           <h2>Add a new Pet!</h2>
           <span>
-            <label htmlFor="pname">Pet Name*: </label>
-            <input type="text" id="pname" name="petname" required alt='Enter Pet Name'></input>
+            <label htmlFor="pname">
+              Pet Name*:&nbsp; 
+              <input 
+                type="text" 
+                id="pname" 
+                name="petname" 
+                required 
+                alt='Enter Pet Name' 
+                value={state.petname}
+                onChange={handleUpdateInput}
+              >
+              </input>
+            </label>
           </span>
           <span>
-            <label htmlFor="ppicture">Profile Picture*: </label>
-            <input type="text" id="ppicture" name="pet picture" placeholder='Picture URL' required alt='Enter Picture URL'></input>
+            <label htmlFor="ppicture">
+              Profile Picture*:&nbsp;  
+              <input
+                type="text" 
+                id="ppicture" 
+                name="petpicture" 
+                placeholder='Picture URL'
+                required 
+                alt='Enter Picture URL'
+                value={state.petpicture}
+                onChange = {handleUpdateInput}
+              >
+              </input>
+            </label>
           </span>
           <fieldset>
             <legend htmlFor="species">Species*</legend>
-            <div className = "legend">
-              <input type ="radio" id ="species1" name = "species" value="cat" required></input>
-              <label htmlFor = "species1">Cat</label>
+                <label htmlFor = "species1">
+                  <input 
+                    type="radio" 
+                    id="species1" 
+                    name="species" 
+                    value={species.Cat}
+                    required
+                    checked={state.species === species.Cat}
+                    onChange={handleUpdateInput}
+                  >
+                  </input>
+                  &nbsp;Cat
+                </label>
+                <br></br>
+              <label htmlFor = "species2">
+                <input 
+                  type="radio" 
+                  id="species2" 
+                  name="species" 
+                  value={species.Dog} 
+                  required
+                  checked={state.species === species.Dog}
+                  onChange={handleUpdateInput}
+                >
+                </input>
+                &nbsp;Dog
+              </label>
               <br></br>
-              <input type ="radio" id ="species2" name = "species" value = "dog" required></input>
-              <label htmlFor = "species2">Dog</label>
+              <label htmlFor = "species3">
+                <input 
+                  type="radio" 
+                  id="species3" 
+                  name="species" 
+                  value={species.Bird}
+                  required                
+                  checked={state.species === species.Bird}
+                  onChange={handleUpdateInput}              
+                >
+                </input>
+                &nbsp;Bird
+              </label>
               <br></br>
-              <input type ="radio" id ="species3" name = "species" value="bird" required></input>
-              <label htmlFor = "species3">Bird</label>
-              <br></br>
-            </div>
           </fieldset>
-            <span>
-              <input type='checkbox' id = "friendly" name='friendly'></input>
-              <label htmlFor = "friendly">Are They Friendly?</label>
-            </span>
-          <button id = "add" name = "addpet">Add Pet!</button>
+            <label htmlFor = "friendly">
+              <input 
+                type="checkbox" 
+                id="friendly" 
+                name="friendly"
+                value={state.friendly}
+                onChange={handleUpdateInput}
+              >
+              </input>
+              &nbsp;Are They Friendly?
+            </label>
+          <button 
+            id = "add" 
+            name = "addpet"
+            onClick={handleAddPet}
+          >
+            Add Pet!
+          </button>
         </form>
         <h2>Pet List</h2>
-        <ul>
-          {
-            pets.map((pet) => {
-              console.log(pet);
-              return (
-                <li key = {pet.id}>
-                  <div className = "card" >
-                  <p><b>{pet.name}</b></p>
-                  <p><img src = {pet.pictureurl} alt='Pet description'></img></p>
-                  <p>{pet.friendly?"friendly":"not friendly"}</p>        
-                  <p>species: {pet.specie.toLowerCase()}</p>
-                  <button id = {"remove"+pet.id} name = "removepet">Remove</button>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
+        <PetList/>
       </main>
     </div>
   );
+}
+function PetList(){
+  const [state, setState] = React.useState({
+    petlist:pets,
+  });
+  function handleRemovePet(){
+    setState({
+      ...state,
+    })
+  }
+  return (
+    <ul name = "petlist">
+      {
+        state.petlist.map((pet) => {
+          return (
+            <li key = {state.petlist.indexOf(pet)}>
+              <div className = "card" >
+              <p><b>{pet.name}</b></p>
+              <p><img src = {pet.pictureurl} alt={pet.name}></img></p>
+              <p>{pet.friendly === undefined ? "" : pet.friendly ?"Friendly!":"Not so friendly..."}</p>        
+              <p>species: {pet.specie}</p>
+              <button 
+                id = {"remove"+pet.name} 
+                name = "removepet"
+                type='button'
+                onClick={() => {pets.splice(pets.indexOf(pet)); console.log(pets); handleRemovePet()}}
+              >
+                Remove
+              </button>
+              </div>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
 }
 export default App;
